bbe62fa4b745e3dd6568acd7b5f74c3c9eaa85d7
Isaac Soltz <isoltz@Isaacs-MacBook-Pro.local>
2020-08-06 20:45:39 -0400

    scrape done, datasets created

---
+++ goalScraper.py
@@ -0,0 +1,21 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+import csv
+
+
+# scraping the goal data from https://www.icydata.hockey/
+for pageCount in range(1,32):
+	filename = f'dataset{pageCount}.csv'
+	url = f'http://www.icydata.hockey/player_stats/{pageCount}/goals'
+	page = requests.get(url)
+
+	with open(filename, 'w+') as csvFile:
+		csvWriter = csv.writer(csvFile)
+		if page.status_code == requests.codes.ok:
+			soup = BeautifulSoup(page.text, 'lxml')
+			goalsTable = soup.find_all('tr')
+			for goal in goalsTable:
+				cols = goal.find_all('td')
+				attributes = [ele.text.strip() for ele in cols]
+				csvWriter.writerow(attributes)
